3be09b15295f90a604a7f9c5359ef7ffa6d530f8
Gustavo M <38445129+foofate@users.noreply.github.com>
2019-09-26 11:10:28 -0300

    Add files via upload

---
+++ DecimalASCII.java
@@ -0,0 +1,25 @@
+// Fa�a um programa que leia um primeiro nome para transformar para o c�digo decimal da tabela ASCII e criar uma senha de 6 caracteres:
+import java.util.Scanner;
+
+public class DecimalASCII {
+  public static void main(String[] args) {
+    int i = 0, soma = 0;
+   
+    Scanner scn = new Scanner(System.in);
+    
+    System.out.println("Digite o seu nome: ");
+    String nome = scn.nextLine();
+    
+    while (i < nome.length()) {
+      System.out.println((int)nome.charAt(i));
+      soma += ((int)nome.charAt(i));
+      i++;
+    }
+    
+    String senha = nome.substring(0, 3) + soma;
+    System.out.println();
+    System.out.println("A sua nova senha é: "+senha);
+    
+
+  }
+}
